1994b153ac42108c4a5c1a3be299ae5192c6ce39
AlambrezFE <andre29flores.aga.nst@gmail.com>
2023-06-08 16:28:10 -0500

    agregar seeder y algunas modificaciones

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Habitacion;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,5 +21,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            AdminSeeder::class
+            ]);
+
+        $this->call([
+            HabitacionesSeeder::class
+            ]);
     }
 }
